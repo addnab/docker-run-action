d94e0f37dbd66885870b328bce3dd1880b7fcf0a
Abdud Dayan Adeeb <addnab@gmail.com>
2020-06-17 19:15:51 -0400

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_USERNAME | docker login $INPUT_REGISTRY -u $INPUT_PASSWORD --password-stdin
 fi
 
+echo $INPUT_COMMAND
+echo "running"
 exec docker run $INPUT_OPTIONS $INPUT_IMAGE $INPUT_COMMAND
