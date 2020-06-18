e8d1c672848079470f8149aa3e350c65b9de9b16
Abdud Dayan Adeeb <addnab@gmail.com>
2020-06-17 22:21:03 -0400

    fix login

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_USERNAME | docker login $INPUT_REGISTRY -u $INPUT_PASSWORD --password-stdin
+then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
 echo "$INPUT_RUN" | sed -e 's/\\n/;/g' > semicolon_delimited_script
