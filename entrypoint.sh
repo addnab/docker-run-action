cf18d277e11e7aaea5619b9a6eee8b8df209945c
Abdud Dayan Adeeb <addnab@gmail.com>
2020-06-17 20:36:04 -0400

    fix: semicolon delimited script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_USERNAME | docker login $INPUT_REGISTRY -u $INPUT_PASSWORD --password-stdin
 fi
 
-echo -e "$INPUT_COMMAND"
-echo "running"
-exec docker run $INPUT_OPTIONS $INPUT_IMAGE $INPUT_COMMAND
+echo $INPUT_COMMAND | sed -e 's/\\n/;/g' > semicolon_delimited_script
+
+exec docker run $INPUT_OPTIONS $INPUT_IMAGE "`cat semicolon_delimited_script`"
