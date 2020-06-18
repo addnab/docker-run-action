940e86ea21036d5688afb0a43e30a9199847336e
Abdud Dayan Adeeb <addnab@gmail.com>
2020-06-17 20:50:44 -0400

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_USERNAME | docker login $INPUT_REGISTRY -u $INPUT_PASSWORD --password-stdin
 fi
 
-echo $INPUT_COMMAND | sed -e 's/\\n/;/g' > semicolon_delimited_script
+echo "$INPUT_COMMAND" | sed -e 's/\\n/;/g' > semicolon_delimited_script
 
 exec docker run $INPUT_OPTIONS $INPUT_IMAGE /bin/sh -c "`cat semicolon_delimited_script`"
