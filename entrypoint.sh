65cfa0ec7a6bfa77232bfd932aa5d6938852111b
Akos Vandra <axos88@gmail.com>
2021-01-05 12:24:27 +0200

    add log of executed docker command

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,5 @@ fi
 
 echo "$INPUT_RUN" | sed -e 's/\\n/;/g' > semicolon_delimited_script
 
+echo docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE "`cat semicolon_delimited_script`"
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE "`cat semicolon_delimited_script`"
