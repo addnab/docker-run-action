c0e88e3a9a83f7fcd711152d2c062c7262a82a90
Jonatan Heyman <jonatan@heyman.info>
2021-01-28 17:13:38 +0100

    Remove whitespace change

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,6 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-
 echo "$INPUT_RUN" | sed -e 's/\\n/;/g' > semicolon_delimited_script
 
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" --network=$INPUT_DOCKER_NETWORK $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "`cat semicolon_delimited_script`"
