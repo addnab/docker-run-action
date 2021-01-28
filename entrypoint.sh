f3a2d182d8b833e3621c8a104099b2e850ac06a9
Jonatan Heyman <jonatan@heyman.info>
2021-01-28 16:22:09 +0100

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
+env
+
 echo "$INPUT_RUN" | sed -e 's/\\n/;/g' > semicolon_delimited_script
 
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "`cat semicolon_delimited_script`"
