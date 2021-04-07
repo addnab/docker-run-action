fe7c510b598e0aefbb77d066d8fd313f544a761a
Dominik Kohler <dominik.kohler@lextira.ch>
2021-04-07 14:41:21 +0200

    use bash "shell parameter expansion"

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,8 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
-echo "$INPUT_RUN" | sed -e 's/\\n/;/g' > semicolon_delimited_script
-
 if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "`cat semicolon_delimited_script`"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
