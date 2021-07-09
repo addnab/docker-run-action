2088b428070dab585be44df889922810ac1da15e
Linas Daneliukas <linas@daneliukas.eu>
2021-07-09 15:12:27 +0300

    Set the correct default workspace for the repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run --workdir $INPUT_WORKDIR -v "/var/run/docker.sock:/var/run/docker.sock" -v "$GITHUB_WORKSPACE:$GITHUB_WORKSPACE" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run --workdir "$INPUT_WORKDIR" -v "/var/run/docker.sock:/var/run/docker.sock" -v "$RUNNER_WORKSPACE:/github/workspace" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
