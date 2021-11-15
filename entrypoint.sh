2dd654d9f4fe7239ef35ccec09fa808ad7e65f58
Yossi Farjoun <farjoun@gmail.com>
2021-11-15 16:09:09 -0500

    require true or false for quote_argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-if [ -z $INPUT_QUOTE_ARGUMENT ];
+if [ $INPUT_QUOTE_ARGUMENT -eq "true" ];
 then exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE $INPUT_SHELL $INPUT_SCRIPT_PREFIX "${INPUT_RUN//$'\n'/;}"
-else exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE $INPUT_SHELL $INPUT_SCRIPT_PREFIX ${INPUT_RUN}
+elif [ $INPUT_QUOTE_ARGUMENT -eq "false" ]; then exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE $INPUT_SHELL $INPUT_SCRIPT_PREFIX ${INPUT_RUN}
+else 
+	echo "Sorry, please specify 'true' or 'false' for the variable quote_argument"
+	exit 1
 fi
