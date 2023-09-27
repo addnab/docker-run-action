b7c1982d92420d38c7012f589cd5cceb3140435b
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-09-27 17:14:29 +0200

    Forward environment files (env variables and mount point)

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,8 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+exec docker run \
+	-v "/var/run/docker.sock":"/var/run/docker.sock" \
+	-v /github/file_commands:/github/file_commands \
+	-e GITHUB_ENV -e GITHUB_OUTPUT -e GITHUB_PATH -e GITHUB_STATE -e GITHUB_STEP_SUMMARY \
+	$INPUT_OPTIONS --entrypoint="$INPUT_SHELL" "$INPUT_IMAGE" -c "${INPUT_RUN//$'\n'/;}"
