d8955c63bfa6c4a26197640d82444bf7c207d3bd
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-10-17 22:39:43 +0200

    Log docker command

--- action.sh
+++ action.sh
@@ -7,6 +7,11 @@ if [ -n "$INPUT_DOCKER_NETWORK" ];
 	then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
+echo "docker run -v \"/var/run/docker.sock\":\"/var/run/docker.sock\" \
+	-v /home/runner/work/_temp:/home/runner/work/_temp \
+	-e GITHUB_ENV -e GITHUB_OUTPUT -e GITHUB_PATH -e GITHUB_STATE -e GITHUB_STEP_SUMMARY \
+	$INPUT_OPTIONS --entrypoint=\"$INPUT_SHELL\" \"$INPUT_IMAGE\" -c \"${INPUT_RUN//$'\n'/;}\""
+
 exec docker run \
 	-v "/var/run/docker.sock":"/var/run/docker.sock" \
 	-v /home/runner/work/_temp:/home/runner/work/_temp \
